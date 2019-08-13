fa50c7210e7a7d866ccb2d0163e2483e67580f72
feneng <354658353@qq.com>
2019-08-13 18:04:21 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using ContosoUniversity.Data;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ContosoUniversity
+{
+    //https://docs.microsoft.com/zh-cn/aspnet/core/data/ef-mvc/crud?view=aspnetcore-2.2
+    //https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/getting-started-with-ef-using-mvc/implementing-basic-crud-functionality-with-the-entity-framework-in-asp-net-mvc-application#overpost
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHost host = CreateWebHostBuilder(args).Build();
+
+            using (IServiceScope scope = host.Services.CreateScope())
+            {
+                IServiceProvider services = scope.ServiceProvider;
+                try
+                {
+                    SchoolContext context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    ILogger<Program> logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
+
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+        }
+    }
+}
